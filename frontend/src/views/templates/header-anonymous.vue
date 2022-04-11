48f4dbf2a2f98ab21136a8284a36c1273f0f6d58
Weizhao Chen <11009259+AnbuShogunate@users.noreply.github.com>
2022-04-10 22:33:26 -0500

    added dropdown

--- frontend/src/views/templates/header-anonymous.vue
+++ frontend/src/views/templates/header-anonymous.vue
@@ -13,17 +13,49 @@
           <!-- <b-nav-item active @click="hide">Active</b-nav-item> -->
           <!-- <b-nav-item href="/home" @click="hide">Home</b-nav-item> -->
           <br>
-          <b-nav-item href="/emr" @click="hide">EMR</b-nav-item>
-          <b-nav-item href="/projectstatus" @click="hide"
+          <!-- Medical related stuff -->
+          <b-nav-item-dropdown
+            id = "medical-dropdown"
+            text="EMR related"
+            toggle-class="nav-link-custom"
+            left
+            >
+            <b-dropdown-item href="/emr" @click="hide">EMR</b-dropdown-item>
+            <b-dropdown-item href="/module" @click="hide">Module</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <br>
+          <!-- Contractor and Project -->
+          <b-nav-item-dropdown
+            id = "contractor/project-dropdown"
+            text="Contractor and Project"
+            toggle-class="nav-link-custom"
+            left
+            >
+            <b-dropdown-item href="/contractor" @click="hide">Contractor</b-dropdown-item>
+            <b-dropdown-item href="/project" @click="hide">Project</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <br>
+          <!-- Contractor and Project Status -->
+          <b-nav-item-dropdown
+            id = "status-dropdown"
+            text="Contractor and Project Status"
+            toggle-class="nav-link-custom"
+            left
+            >
+            <b-dropdown-item href="/contractorstatus" @click="hide">Contractor Status</b-dropdown-item>
+            <b-dropdown-item href="/projectstatus" @click="hide">Project Status</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <!-- <b-nav-item href="/emr" @click="hide">EMR</b-nav-item> -->
+          <!-- <b-nav-item href="/projectstatus" @click="hide"
             >Project Status</b-nav-item
           >
           <b-nav-item href="/contractorstatus" @click="hide"
             >Contractor Status</b-nav-item
-          >
+          > -->
           <br>
-          <b-nav-item href="/module" @click="hide">Module</b-nav-item>
-          <b-nav-item href="/contractor" @click="hide">Contractor</b-nav-item>
-          <b-nav-item href="/project" @click="hide">Project</b-nav-item>
+          <!-- <b-nav-item href="/module" @click="hide">Module</b-nav-item> -->
+          <!-- <b-nav-item href="/contractor" @click="hide">Contractor</b-nav-item>
+          <b-nav-item href="/project" @click="hide">Project</b-nav-item> -->
           <b-nav-item href="/contractorproject" @click="hide"
             >Contractor Project</b-nav-item
           >
@@ -41,6 +73,16 @@
           <br>
           <b-nav-item href="/hospital" @click="hide">Hospital</b-nav-item>
           <br>
+          <!-- Contractor and Project -->
+          <b-nav-item-dropdown
+            id = "contractor/project-dropdown"
+            text="Contractor and Project"
+            toggle-class="nav-link-custom"
+            left
+            >
+            <b-dropdown-item href="/contractor" @click="hide">Contractor</b-dropdown-item>
+            <b-dropdown-item href="/project" @click="hide">Project</b-dropdown-item>
+          </b-nav-item-dropdown>
           <b-nav-item href="/cpreport" @click="hide">Contractor Project Report</b-nav-item>
           <b-nav-item href="/cwreport" @click="hide">Contractor Wisdom Report</b-nav-item>
           <b-nav-item href="/cpmreport" @click="hide">Contractor Preferred Module Report</b-nav-item>
